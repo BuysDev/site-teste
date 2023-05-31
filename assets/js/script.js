a6ed28ea7b0421b10741ed0ad6665b335803a495
Guilherme Buys <131329633+BuysDev@users.noreply.github.com>
2023-05-31 12:14:58 -0300

    Footer
    Começo do footer do site

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const error404 = require('../../index.html')
+
+const handleClick1 = () => {
+    window.location.href = error404
+}
+
+const handleClick2 = () => {
+    window.location.href = error404
+}
+
+const handleClick3 = () => {
+    window.location.href = error404
+}
